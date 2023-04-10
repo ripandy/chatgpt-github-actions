0b42960061f531018809770aecde5bca19a0838f
Ripandy Adha <ripandy.adha@gmail.com>
2023-04-11 06:45:17 +0900

    chore : change to summary and review.

--- main.py
+++ main.py
@@ -12,10 +12,10 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--openai_api_key', help='Your OpenAI API Key')
 parser.add_argument('--github_token', help='Your Github Token')
 parser.add_argument('--github_pr_id', help='Your Github PR ID')
-parser.add_argument('--openai_engine', default="text-davinci-002", help='GPT-3 model to use. Options: text-davinci-002, text-babbage-001, text-curie-001, text-ada-001')
+parser.add_argument('--openai_engine', default="gpt-3.5-turbo", help='GPT model to use.')
 parser.add_argument('--openai_temperature', default=0.5, help='Sampling temperature to use. Higher values means the model will take more risks. Recommended: 0.5')
 parser.add_argument('--openai_max_tokens', default=2048, help='The maximum number of tokens to generate in the completion.')
-parser.add_argument('--mode', default="files", help='PR interpretation form. Options: files, patch')
+parser.add_argument('--file_filter', default="", help='filter file to review')
 args = parser.parse_args()
 
 ## Authenticating with the OpenAI API
@@ -25,70 +25,63 @@ openai.api_key = args.openai_api_key
 g = Github(args.github_token)
 
 
-def files():
-    repo = g.get_repo(os.getenv('GITHUB_REPOSITORY'))
-    pull_request = repo.get_pull(int(args.github_pr_id))
-
-    ## Loop through the commits in the pull request
-    commits = pull_request.get_commits()
-    for commit in commits:
-        # Getting the modified files in the commit
-        files = commit.files
-        for file in files:
-            # Getting the file name and content
-            filename = file.filename
-            content = repo.get_contents(filename, ref=commit.sha).decoded_content
-
-            # Sending the code to ChatGPT
-            response = openai.Completion.create(
-                engine=args.openai_engine,
-                prompt=(f"Explain Code:\n```{content}```"),
-                temperature=float(args.openai_temperature),
-                max_tokens=int(args.openai_max_tokens)
-            )
-
-            # Adding a comment to the pull request with ChatGPT's response
-            pull_request.create_issue_comment(
-                f"ChatGPT's response about `{file.filename}`:\n {response['choices'][0]['text']}")
-
-
-def patch():
+def summary():
     repo = g.get_repo(os.getenv('GITHUB_REPOSITORY'))
     pull_request = repo.get_pull(int(args.github_pr_id))
 
     content = get_content_patch()
-
+    
     if len(content) == 0:
         pull_request.create_issue_comment(f"Patch file does not contain any changes")
         return
 
-    parsed_text = content.split("diff")
+    response = openai.ChatCompletion.create(
+        model=args.openai_engine,
+        messages=[
+            {"role": "system", "content": "You are a helpful code reviewer. \
+                You will provide me summary that consists of \
+                one short and clear paragraph or sentences of the general idea, \
+                followed by short but more detailed explanation in list points."},
+            {"role": "user", "content": "Please summarize this code patch below: \n" + content}],
+        temperature=float(args.openai_temperature),
+        max_tokens=int(args.openai_max_tokens)
+    )
+    print(response)
 
-    for diff_text in parsed_text:
-        if len(diff_text) == 0:
-            continue
+    for choice in response['choices']:
+        print(choice['message']['content'])
+        pull_request.create_issue_comment(f"{choice['message']['content']}")
 
-        try:
-            file_name = diff_text.split("b/")[1].splitlines()[0]
-            print(file_name)
+            
+def review():
+    repo = g.get_repo(os.getenv('GITHUB_REPOSITORY'))
+    pull_request = repo.get_pull(int(args.github_pr_id))
 
-            response = openai.Completion.create(
-                engine=args.openai_engine,
-                prompt=(f"Summarize what was done in this diff:\n```{diff_text}```"),
-                temperature=float(args.openai_temperature),
-                max_tokens=int(args.openai_max_tokens)
-            )
-            print(response)
-            print(response['choices'][0]['text'])
+    content = get_content_patch()
+    
+    if len(content) == 0:
+        pull_request.create_issue_comment(f"Patch file does not contain any changes")
+        return
 
-            pull_request.create_issue_comment(
-                f"ChatGPT's response about ``{file_name}``:\n {response['choices'][0]['text']}")
-        except Exception as e:
-            error_message = str(e)
-            print(error_message)
-            pull_request.create_issue_comment(f"ChatGPT was unable to process the response about {file_name}")
+    response = openai.ChatCompletion.create(
+        model=args.openai_engine,
+        messages=[
+            {"role": "system", "content": "You are a helpful code reviewer. \
+                You will provide me with a list of feedback that consist of \
+                possible bug, possible performance issue, \
+                performance improvement, best practice suggestion, etc. \
+                For each feedback, provide code snippet of what changed, if any."},
+            {"role": "user", "content": "Please review this code patch below: \n" + content}],
+        temperature=float(args.openai_temperature),
+        max_tokens=int(args.openai_max_tokens)
+    )
+    print(response)
 
+    for choice in response['choices']:
+        print(choice['message']['content'])
+        pull_request.create_issue_comment(f"{choice['message']['content']}")
 
+             
 def get_content_patch():
     url = f"https://api.github.com/repos/{os.getenv('GITHUB_REPOSITORY')}/pulls/{args.github_pr_id}"
     print(url)
@@ -103,11 +96,26 @@ def get_content_patch():
     if response.status_code != 200:
         raise Exception(response.text)
 
+    if args.file_filter:
+        diff_lines = response.text.split('\n')
+        filtered_diff_lines = []
+
+        for line in diff_lines:
+            # Look for diff header lines
+            if line.startswith('diff --git'):
+                # Check if the file has the specified extension
+                if line.endswith(args.file_filter):
+                    include_file = True
+                else:
+                    include_file = False
+
+            if include_file:
+                filtered_diff_lines.append(line)
+
+        return '\n'.join(filtered_diff_lines)
+
     return response.text
 
+summary()
 
-if (args.mode == "files"):
-    files()
-
-if (args.mode == "patch"):
-    patch()
+review()
